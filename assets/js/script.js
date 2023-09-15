d685b2b6d11b61e26272db0696ff8eb5138ac85c
hjlee <heunjong.lee@gmail.com>
2023-09-14 22:36:54 -0400

    html modifed for mapquest api test and in js mapquest api test updated to convert route time to hours and minutes. additionally, some spelling errors fixed in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// mapquest API test 
+// mapquest API route time test 
 
 document.getElementById('test-form').addEventListener('submit', function (event) {
     event.preventDefault(); 
@@ -19,15 +19,43 @@ document.getElementById('test-form').addEventListener('submit', function (event)
             // show API response data 
             console.log(data);
 
+            var seconds = data.time[1];
+            // convert seconds to hours (3600s/hr)
+            var hours = Math.floor(seconds / 3600); 
+            // remainder seconds
+            seconds %= 3600;
+            // convert remainder seconds to minutes (60s/min)
+            var minutes = Math.floor(seconds / 60);    
+            
+            var readableTime = '';
+
+            // show hours only if time exceeds one hour
+            if (hours > 0) {
+                readableTime += hours + ' hour'; 
+                if (hours > 1) {
+                    readableTime += 's'; // make hours plural if more than 1 hour
+                }
+                if (minutes > 0) {
+                    readableTime += ' '; // add space if there are minutes to show
+                }
+            }
+
+            // show minutes only if needed
+            if (minutes > 0) {
+                readableTime += minutes + ' minute';
+                if (minutes > 1) {
+                    readableTime += 's'; // make minutes plural if more than 1 min
+                }
+            }
+
             // extract and display the distance from the API response
             if (data.time[1]) {
-                document.getElementById('route-time').textContent = 'Route Time: ' + data.time[1];
+                document.getElementById('route-time').textContent = 'Route Time: ' + readableTime;
             } else {
                 document.getElementById('route-time').textContent = 'Route time not available.';
             }
-
         })
 
 });
 
-// end mapquest API test
+// end mapquest API route time test

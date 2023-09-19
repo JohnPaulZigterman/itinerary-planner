5b446d85d4552921312b001a6759e46bfd562e0c
JohnPaulZigterman <johnpaulzigterman@gmail.com>
2023-09-19 19:05:37 -0400

    added flexbox for results and removed diagnostic outputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,22 +7,19 @@ var city = document.getElementById('city');
 var stateProvince = document.getElementById('state/province');
 var country = document.getElementById('country');
 var submitButton = document.getElementById('submitButton');
-var main = document.getElementById('main');
+var dayContainer = document.getElementById('day-container');
 var scheduleButton = document.getElementById('scheduleButton');
 
 
 
 scheduleButton.addEventListener('click', function (event) {
     event.preventDefault();
-    main.innerHTML += "<p>" + dateStart.value + "</p>";
-    main.innerHTML += "<p>" + dateEnd.value + "</p>";
     var d1 = new Date(dateStart.value);
     var d2 = new Date(dateEnd.value);
     var diff = Math.abs(d1-d2);
-    var days = (diff / 86400000);
-    main.innerHTML += "<p>Days: " + days + "</p>";
+    var days = ((diff / 86400000) + 1);
     for (var i = 1; i <= days; i++) {
-        main.innerHTML += '<section><h2>Day ' + i + '</h2><article><p>Precipitation: <span></span>%</p><p>Temp: <span></span>&deg;F</p><p>Wind: <span></span>mph</p><p>Humidity: <span></span>%</p></article><table class="pure-table pure-table-bordered"><thead><tr><th>Time</th><th>Activity</th></tr></thead><tbody><tr><td></td><td></td></tr><tr><td></td><td></td></tr><tr><td></td><td></td></tr><tr><td></td><td></td></tr><tr><td></td><td></td></tbody></table></section>';
+        dayContainer.innerHTML += '<section><h2>Day ' + i + '</h2><article><p>Precipitation: <span></span>%</p><p>Temp: <span></span>&deg;F</p><p>Wind: <span></span>mph</p><p>Humidity: <span></span>%</p></article><table class="pure-table pure-table-bordered"><thead><tr><th>Time</th><th>Activity</th></tr></thead><tbody><tr><td></td><td></td></tr><tr><td></td><td></td></tr><tr><td></td><td></td></tr><tr><td></td><td></td></tr><tr><td></td><td></td></tbody></table></section>';
     }
 })
 
